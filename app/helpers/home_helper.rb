58d6f408a00ffc68157381cf2b40d7fbe886c0ce
Vadim Tsvid <vadim.tsvid@gmail.com>
2018-12-28 23:47:46 +0200

    move helpers to dedicated moudule

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,15 @@
+module HomeHelper
+  def default_datetime_start(current_datetime_start)
+    (current_datetime_start || DateTime.now).strftime('%Y-%m-%dT%H:%M')
+  end
+
+  def frequency_options(current_frequency)
+    options_for_select(Schedule::PRESETS.keys.map { |k| [k.to_s.humanize, k]},
+                       current_frequency || Schedule::PRESETS.keys.first)
+  end
+
+  def default_items_count(current_count)
+    return '14' if current_count == 0 || current_count.blank?
+    current_count
+  end
+end
